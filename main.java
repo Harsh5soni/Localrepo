7264bd3a0f4f19e5f3f0c9feeca394083c99626c
Student ApnaCollege <sonih4669@gmail.com>
2024-09-12 20:32:45 +0530

    changed

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b, sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the no.");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        sum = a + b;
+        System.out.println(sum);
+    }
+}
